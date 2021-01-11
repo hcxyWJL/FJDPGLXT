6102e4e65f126875f22ae6932d3b7d77004ea3fd
21 <1052967184@qq.com>
2021-01-11 21:00:34 +0800

    类的抽取

--- src/Customer.java
+++ src/Customer.java
@@ -2,17 +2,26 @@ import java.sql.Date;
 
 public class Customer {
     private String id;
-    private  String name;
+    private  String customerType;//旅客类型
+    private  String name; //旅客姓名
     private  String phone;
     private  String cardid; //身份证号
     private Date birthdate; //生日
-    public String getId() {
 
+    public String getId() {
         return id;
     }
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(String customerType) {
+        this.customerType = customerType;
     }
 
     public String getName() {
