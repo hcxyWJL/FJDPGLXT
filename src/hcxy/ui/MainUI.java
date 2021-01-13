5fc1c550feb6c8e1f5da443173dfce4ce886946d
21 <1052967184@qq.com>
2021-01-13 20:00:21 +0800

    实现了功能（1）录入航班信息和功能（2）查询航班信息

--- src/hcxy/ui/MainUI.java
+++ src/hcxy/ui/MainUI.java
@@ -5,11 +5,16 @@ import hcxy.bll.IFlightService;
 import hcxy.bll.impl.FlightServiceImpl;
 import hcxy.dao.IFlightdao;
 
+import java.sql.SQLException;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainUI {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
         while (true)
         {
@@ -19,26 +24,18 @@ public class MainUI {
             System.out.println("按4，预定机票");
             System.out.println("按5，退票");
             System.out.println("按6，退出系统");
-
-            if (sc.nextInt()==1)
+            int choice=sc.nextInt();
+            if (choice==1)
             {
-              /*  private String  id;
-                private  String flightid;
-                private  String planeType;//机型
-                 private  String currentseatsnum;//目前座位数
-                private  String startairport; //起飞机场
-                private  String arrivalairport;//到达机场
-                private  String starTime; //起始时间
 
-               */
                 String id= UUID.randomUUID().toString();
-                id.replaceAll("-","");
+               id=id.replaceAll("-","");
                 System.out.print("航班号：");
                 String flightid=sc.next();
                 System.out.print("机型：");
                 String planeType=sc.next();
                 System.out.print("目前座位数：");
-                String currentseatsnum=sc.next();
+                int currentseatsnum=sc.nextInt();
                 System.out.print("起飞机场：");
                 String startairport=sc.next();
                 System.out.print("目的机场：");
@@ -46,11 +43,50 @@ public class MainUI {
                 System.out.print("起飞时间：");
                 String starTime=sc.next();
 
-                Flight flight = new Flight(flightid,planeType,currentseatsnum,startairport,arrivalairport,starTime);
+                Flight flight = new Flight(id,flightid,planeType,currentseatsnum,startairport,arrivalairport,starTime);
                 IFlightService iFlightService =new FlightServiceImpl();
-                iFlightService.insertFlight(flight);
+                try {
+                    iFlightService.insertFlight(flight);
+                } catch (SQLException e) {
+                    String errorMessage = e.getMessage();
+                    if (errorMessage.startsWith("ORA-12899")) {
+                        //ORA-12899: value too large for column "OPTS"."FLIGHT"."ID" (actual: 32, maximum: 30)
+                      // String pattern = "(\\w+-\\d{5}):(\\s\\w+)+\\s(\"\\w+\")\\.(\"\\w+\")\\.(\"\\w+\")";
+                        // ORA-12899: 列 "OPTS"."FLIGHT"."ID" 的值太大 (实际值: 36, 最大值: 32)
+                        // String pattern="(\\w+-\\d{5}):(\\W)+\\s(\"\\w+\")\\.(\"\\w+\")\\.(\"\\w+\")";
+                        //\W	匹配任何非单词字符.
+                       String pattern="(\\W)+(\"\\w+\")\\.(\"\\w+\")\\.(\"\\w+\")";
+                       //pattern="( ORA-12899: 列 )("OPTS").("FLIGHT").("ID")";
+                        // 创建 Pattern 对象
+                        Pattern r = Pattern.compile(pattern);
+                        // 现在创建 matcher 对象
+                        Matcher m = r.matcher(errorMessage);
+                        if (m.find()) {
+
+                            String tableName = m.group(3);
+                            String columnName = m.group(4);
+                            System.out.println(tableName + "表的" + columnName + "这一列的值过大，请仔细检查");
+                        } else {
+                            System.out.println("NO MATCH");
+                        }
+                    }
+                }
             }
-            else if (sc.nextInt()==6)
+            else if (choice==2)
+            {
+                IFlightService iFlightService =new FlightServiceImpl();
+                try {
+                    Set<Flight> allFlights=iFlightService.getAllFlights();
+                    for(Flight flight:allFlights)
+                    {
+                        System.out.println(flight);
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            else if (choice==6)
             {
                 System.out.println("系统退出！");
                 break;
